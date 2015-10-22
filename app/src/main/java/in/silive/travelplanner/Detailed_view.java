a4585c6829e4624365dd5c7c7dc7a7ea10bd5c23
Akriti <akritikts@gmail.com>
2015-10-22 06:51:49 +0530

    Display-Detaild View

--- app/src/main/java/in/silive/travelplanner/Detailed_view.java
+++ app/src/main/java/in/silive/travelplanner/Detailed_view.java
@@ -4,10 +4,12 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class Detailed_view extends AppCompatActivity {
     Cursor cursor;
+    DataBaseHelper db=new DataBaseHelper(this);
     Bundle info;
     Integer pos;
     TextView date_txtview;
@@ -21,25 +23,29 @@ public class Detailed_view extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detailed_view);
-        display_details();
-    }
-
-    public void display_details() {
-        Intent i=getIntent();
-        pos = Integer.parseInt(i.getStringExtra("position"));
-        cursor.moveToPosition(pos);
+        cursor=db.getDetailedJourney();
+        cursor.moveToFirst();
         date_txtview = (TextView) findViewById(R.id.date_txtview);
         source = (TextView) findViewById(R.id.source);
         destination = (TextView) findViewById(R.id.destination);
         train = (TextView) findViewById(R.id.train);
         hotel = (TextView) findViewById(R.id.hotel);
         fav = (TextView) findViewById(R.id.fav);
-        date_txtview.setText(cursor.getString(0));
+        display_details();
+    }
+
+    public void display_details() {
+        Log.d("display","detailed");
+        Intent i=getIntent();
+        pos = Integer.parseInt(i.getStringExtra("position"));
+        cursor.moveToPosition(pos);
+        date_txtview.setText(cursor.getString(3));
         source.setText(cursor.getString(1));
         destination.setText(cursor.getString(2));
-        train.setText(cursor.getString(3));
-        hotel.setText(cursor.getString(6));
-        fav.setText(cursor.getString(7));
+        train.setText(cursor.getString(4));
+        hotel.setText(cursor.getString(8));
+        fav.setText(cursor.getString(9));
+        Log.d("display", "detailed workd");
 
 
     }
